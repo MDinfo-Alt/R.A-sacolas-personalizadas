2c051e28e2ac0f517f5999129a31e6122a69da52
Marlon Dias <81657991+MDinfo-Alt@users.noreply.github.com>
2025-12-13 21:44:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const track = document.querySelector(".carousel-track");
+  const images = track.querySelectorAll("img");
+  const prevBtn = document.querySelector(".carousel-btn.prev");
+  const nextBtn = document.querySelector(".carousel-btn.next");
+
+  let index = 0;
+  const visible = 3; // quantas imagens aparecem por vez
+
+  function updateCarousel() {
+    const imageWidth = images[0].offsetWidth + 20; // largura + gap
+    track.style.transform = `translateX(-${index * imageWidth}px)`;
+  }
+
+  nextBtn.addEventListener("click", () => {
+    if (index < images.length - visible) {
+      index++;
+      updateCarousel();
+    }
+  });
+
+  prevBtn.addEventListener("click", () => {
+    if (index > 0) {
+      index--;
+      updateCarousel();
+    }
+  });
+
+  window.addEventListener("resize", updateCarousel);
+});
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add("show");
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
